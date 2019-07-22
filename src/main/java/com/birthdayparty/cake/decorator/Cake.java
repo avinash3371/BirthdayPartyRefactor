c97beac0d3824c4b13d07fcf61a1811351ba5e61
avinash.shende <avinash.shende@fisglobal.com>
2019-07-21 21:20:07 -0700

    Birthday party refactoring problem simplification

--- src/main/java/com/birthdayparty/cake/decorator/Cake.java
+++ src/main/java/com/birthdayparty/cake/decorator/Cake.java
@@ -10,4 +10,8 @@ public interface Cake {
 	CakeSize getSize();
 
 	CakeColor getColor();
+	
+	
+	//If we want to add some new behaviour in feature then we can use java 8 default methods , 
+	// otherwise we need to make this as  abtract class
 }
