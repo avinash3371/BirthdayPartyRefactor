21c2512100e91c5c3d1f7036fdb1663fb0d5bbf6
avinash.shende <avinash.shende@fisglobal.com>
2019-07-22 16:21:56 -0700

    Birthday party refactoring problem simplification- main metjod update

--- src/main/java/scrap/heap/refactor/App.java
+++ src/main/java/scrap/heap/refactor/App.java
@@ -1,17 +1,34 @@
 package scrap.heap.refactor;
 
+import com.birthdayparty.balloon.builder.Balloon;
+import com.birthdayparty.balloon.builder.BalloonColor;
+import com.birthdayparty.balloon.builder.BalloonMaterial;
+import com.birthdayparty.cake.decorator.Cake;
+import com.birthdayparty.cake.decorator.FullSheetSizeDecorator;
+import com.birthdayparty.cake.decorator.MangoMouseWithStrawBerryFrostingCake;
+import com.birthdayparty.cake.decorator.StandardCake;
+import com.birthdayparty.order.BirthdayOrder;
+import com.birthdayparty.order.BirthdayOrderProcessor;
+import com.birthdayparty.order.OrderProcessingException;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws OrderProcessingException {
 
-         //Place birthday party orders
-         order("red", "mylar", "4", "chocolate", "chocolate", "circle", "large", "brown" );
-         order("blue", "latex", "7", "Vanilla", "chocelate", "square", "med", "brown" );
-         order("yellow", "mylar", "4", "vanilla", "vanilla", "square", "small", "yellow" );
+    	BirthdayOrder birthdayOrder = new BirthdayOrderProcessor();
 
+		Balloon cartItem1 = new Balloon.BalloonBuilder(BalloonColor.RED, BalloonMaterial.MYLAR).build();
+		birthdayOrder.orderBalloons(cartItem1, 4);
+
+		Balloon cartItem2 = new Balloon.BalloonBuilder(BalloonColor.BLUE, BalloonMaterial.LATEX).fillWithHeliumGas()
+				.build();
+		birthdayOrder.orderBalloons(cartItem2, 7);
+
+		Cake mangoCake = new FullSheetSizeDecorator(new MangoMouseWithStrawBerryFrostingCake(new StandardCake()));
+		birthdayOrder.orderCake(mangoCake);
     }
 
     private static void order(String balloonColor, String material, String number, String flavor, String frostingFlavor, String shape, String size, String cakeColor){
